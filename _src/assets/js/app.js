bf8e67d760eb900be8a88b2877066773662b156d
Mike Aparicio <peruvianidol@gmail.com>
2022-02-01 21:07:04 -0600

    Background gradient

--- _src/assets/js/app.js
+++ _src/assets/js/app.js
@@ -2,5 +2,5 @@ function selectGradient() {
   let select = document.querySelector("#sceneBackground");
   let scene = document.querySelector("#scene");
   
-  scene.classList.add("og-gradient-" + select.value);
+  scene.setAttribute("data-gradient", select.value);
 }
