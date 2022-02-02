33511055798619e72cd908524c6c81b864406fc4
Mike Aparicio <peruvianidol@gmail.com>
2022-02-01 23:32:13 -0600

    reset input after setting the other

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,15 +1,18 @@
 function selectGradient() {
-  let select = document.querySelector("#sceneGradient");
+  let colorPicker = document.querySelector("#sceneBgColor");
+  let gradientSelect = document.querySelector("#sceneGradient");
   let scene = document.querySelector("#scene");
   scene.style.setProperty("data-bg-color", "");
-  scene.setAttribute("data-gradient", select.value);
+  colorPicker.value = "#000000";
+  scene.style.setProperty("--bg-color", "");
+  scene.setAttribute("data-gradient", gradientSelect.value);
 }
 
 function selectBgColor() {
-  let input = document.querySelector("#sceneBgColor");
-  let select = document.querySelector("#sceneGradient");
+  let colorPicker = document.querySelector("#sceneBgColor");
+  let gradientSelect = document.querySelector("#sceneGradient");
   let scene = document.querySelector("#scene");
   scene.setAttribute("data-gradient", "");
-  select.value = '';
-  scene.style.setProperty("--bg-color", input.value);
+  gradientSelect.value = '';
+  scene.style.setProperty("--bg-color", colorPicker.value);
 }
