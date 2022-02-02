85562aaa76a19b5acaff3a015865b98acb625e4e
Mike Aparicio <peruvianidol@gmail.com>
2022-02-01 23:27:29 -0600

    Change bg color OR gradient

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,6 +1,15 @@
 function selectGradient() {
-  let select = document.querySelector("#sceneBackground");
+  let select = document.querySelector("#sceneGradient");
   let scene = document.querySelector("#scene");
-  
+  scene.style.setProperty("data-bg-color", "");
   scene.setAttribute("data-gradient", select.value);
+}
+
+function selectBgColor() {
+  let input = document.querySelector("#sceneBgColor");
+  let select = document.querySelector("#sceneGradient");
+  let scene = document.querySelector("#scene");
+  scene.setAttribute("data-gradient", "");
+  select.value = '';
+  scene.style.setProperty("--bg-color", input.value);
 }
